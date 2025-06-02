529212d281080157e1b456092de90958fda204f6
Amanda Wiggins <wiggins.amanda@gmail.com>
2025-06-02 11:27:29 -0700

    added basic js and added typewriter plugin

---
+++ index.js
@@ -0,0 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#typewriter", {
+    strings: ["La tombe dit a la rose"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
