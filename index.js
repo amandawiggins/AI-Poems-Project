6611fffd7c6b811e91b6811df6dabf995e934c5b
Amanda Wiggins <wiggins.amanda@gmail.com>
2025-06-02 12:05:02 -0700

    added more code for ai context and added api url and added axios

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#typewriter", {
     strings: ["La tombe dit a la rose"],
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "f8bb49552a205a50dct88eo7f9d4363c";
+  let prompt = `generate a french poem about ${instructionsInput.value}`;
+  let context = "";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  console.log("generating-poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
