95188799e8e352fc0e7739468df09e3419fcbf31
Amanda Wiggins <wiggins.amanda@gmail.com>
2025-06-03 11:52:26 -0700

    making app better for ux

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "f8bb49552a205a50dct88eo7f9d4363c";
-  let prompt = `generate a french poem about ${instructionsInput.value}`;
-  let context = "";
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions.";
+  let prompt = `User-instructions: Generate a french poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
-  console.log("generating-poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating your poem ${instructionsInput.value}`;
 
   axios.get(apiUrl).then(displayPoem);
 }
